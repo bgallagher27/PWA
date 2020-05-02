1ddb953f3824bda07b047532d0697a9e4dac25ff
Brendan Gallagher <bpgallagher27@gmail.com>
2020-05-01 20:21:56 -0400

    set up file caching

---
+++ public/service-worker.js
@@ -0,0 +1,11 @@
+const FILES_TO_CACHE = [
+   "/",
+   "/db.js",
+   "/index.html",
+   "/index.js",
+   "/manifest.webmanifest",
+   "/style.css"
+];
+
+const CACHE_NAME = "static-cahce-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
